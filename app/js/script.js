1615f21a7f310a39bedf8a4d7450a7a63ed5e23a
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-13 00:15:04 -0700

    Dark/light toggle with implemented state

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,55 @@
-const radioButtons = document.querySelectorAll('.toggle__wrapper input');
-for (let i = 0; i < radioButtons.length; i++) {
-    radioButtons[i].addEventListener('click', event => {
-        document.getElementById('dark').checked 
-        ? (document.querySelector('body').classList = 'dark') 
-        : (document.querySelector('body').classList = 'light');
-    })
-}
+const radioButtons = document.querySelectorAll(".toggle__wrapper input");
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
 
+const setColorMode = () => {
+  if (localStorage.getItem("colorMode") == "dark") {
+    setDarkMode();
+    darkButton.click();
+  } else if (localStorage.getItem("colorMode") == "light") {
+    setLightMode();
+    lightButton.click();
+  }
+};
+
+const setDarkMode = () => {
+  document.querySelector("body").classList = "dark";
+};
+
+const setLightMode = () => {
+  document.querySelector("body").classList = "light";
+};
+
+const checkModeChange = () => {
+  window
+    .matchMedia("(prefers-color-scheme:dark)")
+    .addEventListener("change", (event) => {
+      checkMode();
+    });
+};
+
+const checkMode = () => {
+  if (localStorage.getItem("colorMode") == null) {
+    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+      lightButton.click();
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      darkButton.click();
+    }
+  }
+};
+
+setColorMode();
+checkMode();
+checkModeChange();
+
+for (let i = 0; i < radioButtons.length; i++) {
+  radioButtons[i].addEventListener("click", (event) => {
+    if (darkButton.checked) {
+      localStorage.setItem("colorMode", "dark");
+      setDarkMode();
+    } else {
+      localStorage.setItem("colorMode", "light");
+      setLightMode();
+    }
+  });
+}
